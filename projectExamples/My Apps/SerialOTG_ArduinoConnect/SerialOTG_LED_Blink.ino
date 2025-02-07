340c0961d466740385fd614f576a9741fde5735c
Golam Mostofa <mostofa.melab@gmail.com>
2025-02-07 23:04:59 +0600

    Added App Inventor with Arduino via Serial OTG
    Added example folder for App Inventor with Arduino via Serial OTG

---
+++ projectExamples/My Apps/SerialOTG_ArduinoConnect/SerialOTG_LED_Blink.ino
@@ -0,0 +1,24 @@
+/*
+ * Test program, echo serial in 
+ * if "1", LED on. if "0" LED off
+ */
+#define led 8
+// #define led 17          //ProMicro Only blink
+ 
+ void setup()  
+ {  
+  Serial.begin(9600);  
+  pinMode(led, OUTPUT);
+ } 
+  
+ void loop()  
+ {  
+  char c;
+  if(Serial.available())  
+  {  
+   c = Serial.read();  
+     Serial.print(c);  
+     if (c==0x31) digitalWrite(led, HIGH);
+     if (c==0x30) digitalWrite(led, LOW);     
+  }  
+ }  
